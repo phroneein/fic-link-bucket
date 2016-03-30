90867d84252ce2cecc8aaa110ff1e440cb464144
phroneein <phamstudent@gmail.com>
2016-03-30 01:39:00 -0500

    Gets link HTML based on author input by user

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-chrome.browserAction.setBadgeText({text: "ao3.L"});
+chrome.browserAction.setBadgeText({text: "ao3.L"}); //text on the icon 
 
 chrome.browserAction.onClicked.addListener(function() {
   chrome.tabs.create({url: "content.html"}, function(tab) {
