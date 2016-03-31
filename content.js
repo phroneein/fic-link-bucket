fd9f1a4e66a2a32842d8920dd5b5abbb1dddb67e
phroneein <phamstudent@gmail.com>
2016-03-31 13:00:47 -0500

    Make printList function, number printed links
    clean code, edit page description

--- content.js
+++ content.js
@@ -10,6 +10,18 @@ function sendRequest(url, callback) {
 	xhr.send();
 }
 
+function printList(response) {
+	var linkHTML = document.getElementById('HTMLfromLink');
+	var el = document.createElement('html');
+	el.innerHTML = response;
+	var nl = el.getElementsByTagName('a');// Live NodeList of anchor elements
+	var arr = Array.prototype.slice.call(nl);
+	var printLL = document.getElementById('printLinkList');
+	for (index = 0; index < arr.length; index++) { //DEBUG: replaced "arr.length" w/ 10
+		printLL.innerHTML += index+1 + '. ' + arr[index] + '<br>'; //To-do: remove 'arr[index]'s that start with chrome.runtime.id = .substring(chrome.runtime.id.length)
+	}
+}
+
 // the function which handles the input field logic
 function getUserName() {
 	var nameField = document.getElementById('nameField').value;
@@ -24,20 +36,10 @@ function getUserName() {
 	
 	var Printao3AuthorURL = document.getElementById('printAuthorURL');
 	Printao3AuthorURL.textContent = 'Author URL: ' + ao3LinkToAuthor;
+	//Get HTML from URL based on input
 	sendRequest(ao3LinkToAuthor, function (response) {
-		var linkHTML = document.getElementById('HTMLfromLink');
-		//////////linkHTML.textContent = 'Your request returned this: <xmp>' + response + '</xmp>';
-		////////// <xmp>response</xmp> --> so it will print out the XML code instead of rendering
-		var el = document.createElement('html');
-		el.innerHTML = response;
-		var nl = el.getElementsByTagName('a');// Live NodeList of anchor elements
-		var arr = Array.prototype.slice.call(nl);
-		var printLL = document.getElementById('printLinkList');
-		for (index = 0; index < arr.length; index++) { //DEBUG: replaced "arr.length" w/ 10
-//		  document.writeln(arr[index] + '<br>'); //writes w/ newline after text
-			printLL.innerHTML += arr[index] + '<br>';
-			//To-do: remove 'arr[index]'s that start with chrome.runtime.id = .substring(chrome.runtime.id.length)
-		}
+		//Parse HTML for links, and print in list
+		printList(response);
 	});
 }
 // use an eventlistener for the event
