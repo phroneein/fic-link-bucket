12d7107c19d22579205d2dea1178184b1d3c0327
phroneein <phamstudent@gmail.com>
2016-03-30 23:27:25 -0500

    Prints parsed links from HTML to list on tab
    Bug: some links are printing with Extension link instead of URL link

--- content.js
+++ content.js
@@ -1,3 +1,4 @@
+// get HTML from webpage URL
 function sendRequest(url, callback) {
 	var xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function () {
@@ -7,6 +8,31 @@ function sendRequest(url, callback) {
 	};
 	xhr.open("GET", url, true);
 	xhr.send();
+}
+
+function ShowResults(value){
+	alert(index);
+}
+
+function getLinks(response){
+	var el = document.createElement('html');
+	el.innerHTML = response;
+	var nl = el.getElementsByTagName('a');// Live NodeList of anchor elements
+  var arr = [];
+  for(var i = nl.length; i--; arr.unshift(nl[i]));
+	linklist.textContent = 'Array: ' + arr;
+	alert(arr);
+////	x = Array.prototype.slice.call(x);
+////	var fullLinkList = x.foreach(ShowResults);
+////	linklist.textContent = 'All links:  ' + fullLinkList;
+	//var linklist = document.getElementById('printLinkList');
+	//linklist.textContent = el.getElementsByTagName('a');
+//	var i;
+//	var linklist = document.getElementById('printLinkList');
+	/*for (i=0; i<x.length; i++) [
+		linklist.textContent = x[i] + '\n';
+	}*/
+//	linklist.textContent = 'First element: ' + x[i];
 }
 
 // the function which handles the input field logic
@@ -26,9 +52,23 @@ function getUserName() {
 	var Printao3AuthorURL = document.getElementById('printAuthorURL');
 	Printao3AuthorURL.textContent = 'Author URL: ' + ao3LinkToAuthor;
 	sendRequest(ao3LinkToAuthor, function (response) {
+		//getLinks(response);
 		var linkHTML = document.getElementById('HTMLfromLink');
-		linkHTML.textContent = 'Your request returned this: <xmp>' + response + '</xmp>';
-		// <xmp>response</xmp> --> so it will print out the XML code instead of rendering
+		//////////linkHTML.textContent = 'Your request returned this: <xmp>' + response + '</xmp>';
+		////////// <xmp>response</xmp> --> so it will print out the XML code instead of rendering
+		
+		var el = document.createElement('html');
+		el.innerHTML = response;
+		var nl = el.getElementsByTagName('a');// Live NodeList of anchor elements
+		var arr = Array.prototype.slice.call(nl);
+		/*for (var index = 0; index < nl.length; index++) {
+			alert(nl[index]);//works -- pop up with URL
+		}*/
+		for (index = 0; index < arr.length; index++) {
+		  document.write(arr[index] + '<br><br>');
+		}
+		
+		linklist.textContent = 'Array: ' + arr;
 	});
 }
 // use an eventlistener for the event
